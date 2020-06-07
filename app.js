1b8470f11b2df910986e5956f5c875a218b169d6
HsnRao0335 <hassanribery0335@gmail.com>
2020-06-08 02:44:51 +0500

    Assignment#2 completed

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 // ALERTS
 
-// TASK #1
+//  #1
 alert("Welcome");
 
-// TASK#2
+// #2
 var input = prompt("Please enter your password");
 var password = "Hassan";
 if (password === input) {
@@ -12,12 +12,42 @@ if (password === input) {
   alert("Error! please enter a valid password");
 }
 
-// TASK#3
+// #3
 alert("Welcome to JS land...\n Happy Coding!");
 
-// TASK#4
+// #4
 var a = alert("Weclome to Js land...");
 var a = alert("Happy Coding!");
 
-// TASK#5
+// #5
 console.log("Hello... i can run JS through my web browser's console");
+
+// VARIABLES FOR STRING
+// #1
+var username;
+// #2
+var myName = "Mohammad Hassan Rao";
+// #3
+var message;
+message = "Hello World";
+alert(message);
+// #4
+var Name = prompt("Enter your name");
+
+var age = prompt("Your Age?");
+
+var qual = prompt("Your Qualification?");
+alert(Name + "\n" + age + "\n" + qual);
+// #5
+var food = "PIZZA\nPIZZ\nPIZ\nPI\nP";
+alert(food);
+// #6
+var email = "hassanribery0335@gmail.com";
+alert("My email address is " + email);
+// #6
+var book = "A smarter way to learn JS";
+alert("I am trying to learn from the Book" + book);
+// #7
+document.write("Yah! I can write HTML content through JAVASCRIPT");
+// #8
+alert("▬▬▬▬▬▬▬▬▬ஜ۩END۩ஜ▬▬▬▬▬▬▬▬▬");
