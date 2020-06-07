6faa6fd53a0d4ffd9b70c8530e8a0ad9bdb58699
HsnRao0335 <hassanribery0335@gmail.com>
2020-06-08 02:15:50 +0500

    Assignment#1 Completed

--- app.js
+++ app.js
@@ -1,3 +1,23 @@
 // ALERTS
 
 // TASK #1
+alert("Welcome");
+
+// TASK#2
+var input = prompt("Please enter your password");
+var password = "Hassan";
+if (password === input) {
+  alert("welcome back");
+} else {
+  alert("Error! please enter a valid password");
+}
+
+// TASK#3
+alert("Welcome to JS land...\n Happy Coding!");
+
+// TASK#4
+var a = alert("Weclome to Js land...");
+var a = alert("Happy Coding!");
+
+// TASK#5
+console.log("Hello... i can run JS through my web browser's console");
